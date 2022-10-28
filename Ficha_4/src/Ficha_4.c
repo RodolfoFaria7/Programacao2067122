5d4a4b01296b3e914706bb8c3acf29f030f0734b
RodolfoFaria7 <2067122@student.uma.pt>
2022-10-28 19:49:07 +0100

    Ficha_4

---
+++ Ficha_4/src/Ficha_4.c
@@ -0,0 +1,19 @@
+/*
+ ============================================================================
+ Name        : Ficha_4.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	for(int i = 0; i < 10; i+=2) {
+			printf ("%i ", i);
+		}
+	return 0;
+}
