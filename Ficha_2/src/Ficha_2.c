bf1dc586875ba9ad9af53bea5b92db3e77e9a95d
RodolfoFaria7 <2067122@student.uma.pt>
2022-10-28 18:04:06 +0100

    Ficha_2

---
+++ Ficha_2/src/Ficha_2.c
@@ -0,0 +1,66 @@
+/*
+ ============================================================================
+ Name        : ficha2.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(void) {
+	//========================== Alinea 1 ====================================
+	/*
+	float p1 = 16;
+	float p2 = 18;
+	float freq = 19;
+	float ntf = (p1*0.3)+(p2*0.3)+(freq*0.4);
+	printf ("A nota final foi : %f", ntf);
+	return 0;
+*/
+	//========================== Alinea 2 ====================================
+	/*
+	float altura = 10;
+	float base = 17;
+	float area = (base*altura);
+	printf("A area e : %f", area);
+	*/
+	//========================== Alinea 3 ====================================
+	/*
+	float c = 20;
+	float l = 15;
+	float h = 12;
+	float v = (c*l*h);
+	printf("Volume e : %f", v);
+	*/
+	//========================== Alinea 4 ====================================
+	/*
+	float dist = 370;
+	float consumo = 40;
+	float km = 100;
+	float mediapl = (km*consumo)/dist;
+	printf ("A madia do seu veiculo aos %f e %f", km, mediapl);
+	*/
+	//========================== Alinea 5 ====================================
+	/*
+	int x1 = 2;
+	int y1 = 1;
+	int x2 = 5;
+	int y2 = 3;
+	//float x = pow(x2-x1, 2);
+	//float y = pow(y2-y1, 2);
+	float distance = sqrt(pow(x2-x1, 2) + pow(y2-y1, 2));
+	printf ("%f", distance);
+	*/
+	//========================== Math.h ====================================
+	/*
+	float ff = sqrt(4);
+	float esp = pow(2,3);
+	*/
+	//======================================================================
+	return 0;
+}
