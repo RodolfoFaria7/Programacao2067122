013fca9653dbb87fb8507c2f20bc51cd6de4a32f
RodolfoFaria7 <2067122@student.uma.pt>
2022-11-04 19:50:36 +0000

    Codigo teste

---
+++ test/src/test.c
@@ -0,0 +1,24 @@
+/*
+ ============================================================================
+ Name        : test.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	int a, b;
+	int c;
+
+	a = 10;
+	b = 20;
+	c = a + b;
+
+	printf ("Valor de a = %d, b = %d, e c = %d\n", a, b ,c);
+	return EXIT_SUCCESS;
+}
